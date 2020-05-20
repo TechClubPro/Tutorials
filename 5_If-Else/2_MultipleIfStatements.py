084b8be604de5aa2a8269fb69a930847a5aad140
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2020-05-20 12:19:55 +0530

    Add files via upload

---
+++ 5_If-Else/2_MultipleIfStatements.py
@@ -0,0 +1,19 @@
+"""
+Program to check 
+whether the number entered by the user is greater than 100 
+"""
+
+price = int(input("Enter the Price of a Product: "))
+
+#Compare the Price 
+#check whether Price > 100
+
+if price > 100:
+    print("Your product is costly")
+    print(price)
+    diff = price -100
+    print("The Difference in price is : " + str(diff))
+    
+if price < 100:
+    print("I am buying the product!!")
+    print("Price is in my budget")
