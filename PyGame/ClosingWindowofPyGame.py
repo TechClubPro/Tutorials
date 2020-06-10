a0468f220e477a1763eb32fdc4e432b3e865874c
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2020-06-10 13:14:55 +0530

    Add files via upload

---
+++ PyGame/ClosingWindowofPyGame.py
@@ -0,0 +1,62 @@
+"""
+PyGame Library for Displaying Image
+"""
+
+""" Import Lib"""
+import pygame
+
+"""Variable for Brightness of Image"""
+bright=100
+
+""" Initialize Pygame"""
+pygame.init()
+
+
+""" Set the Dimension of the Screen"""
+Width = 1000
+Height = 1000
+
+""" Set the Screen"""
+screen = pygame.display.set_mode((Width,Height))
+
+
+""" Set The Title of Screen"""
+pygame.display.set_caption("Display Image")
+
+
+""" Load the Image"""
+screenImg = pygame.image.load("Images/Space.jpg")
+
+""" Display Image at Specific Co-Ordinate"""
+
+screen.blit(screenImg,(0,0))
+
+""" Update the Display Continuously"""
+
+EventStatus = "None"
+
+while True:
+    
+           
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            EventStatus="Quit"
+            break
+       
+        
+    if EventStatus == "Quit":
+        break
+    
+print("Closing")
+    
+    
+        
+    
+     
+            
+        
+
+    
