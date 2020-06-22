50a82358096da70fae1700f8699a6391db701b67
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2020-06-22 11:06:28 +0530

    Add files via upload

--- PyGame/DisplayImage.py
+++ PyGame/DisplayImage.py
@@ -13,8 +13,8 @@ pygame.init()
 
 
 """ Set the Dimension of the Screen"""
-Width = 800
-Height = 600
+Width = 1000
+Height = 1000
 
 """ Set the Screen"""
 screen = pygame.display.set_mode((Width,Height))
@@ -40,46 +40,7 @@ while True:
            
     pygame.display.update()
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            EventStatus="Quit"
-            break
-        if event.type == pygame.KEYDOWN:
-           if event.key==pygame.K_x:
-               screenImg= pygame.image.load("Images/Sky.jpg")
-               screen.blit(screenImg,(0,0))
-               pygame.mixer.Sound("audio/Bell.wav")
-               
-           if event.key==pygame.K_y:
-               screenImg= pygame.image.load("Images/Earth.jpg")
-               screen.blit(screenImg,(0,0))
-               
-           if event.key==pygame.K_RIGHT: # This is the Right Arrow Key on Keyborad
-               
-               #Incrementing the brightness from 100 - 255 in steps of 20
-               bright+=20
-              
-               if bright>=255:
-                   bright=255
-               
-               screenImg= pygame.image.load("Images/Space.jpg")
-               
-               #Function to adjust brightness of the image
-               screenImg.fill((bright,bright,bright),special_flags=pygame.BLEND_RGB_ADD)
-               screen.blit(screenImg,(0,0))
-              
-              
-           if event.key==pygame.K_h:
-               screenImg= pygame.image.load("Images/Space.jpg")
-               screen.blit(screenImg,(0,0))
 
-        
-    if EventStatus == "Quit":
-        break
-    
-print("Closing")
-    
     
         
     
